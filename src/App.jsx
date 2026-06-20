9cdd2771087b64b447ca8685162f912580155417
mito-coder <abhishek.prajapati.it@gmail.com>
2026-06-20 15:40:21 +0530

    add Home page hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import Navbar from "./components/Navbar";
+import Home from "./Pages/Home";
 
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
 }
 
 export default App;
---
+++ src/Pages/Home.jsx
@@ -0,0 +1,27 @@
+function Home() {
+  return (
+    <div>
+      {/* Hero Section */}
+
+      <div className="bg-blue-600 text-white text-center py-20 px-6">
+        <h1 className="text-4xl font-bold mb-4">Find Your Dream Job</h1>
+
+        <input
+          className="px-4 py-2 rounded-l-lg w-80 text-black outline-none"
+          type="text"
+          placeholder="Search from thousands of jobs"
+        />
+
+        <button className="bg-yellow-400 text-black px-6 py-2 rounded-r-lg font-bold">
+          Search
+        </button>
+      </div>
+
+      {/* Categories section */}
+
+      {/* Recent job section */}
+    </div>
+  );
+}
+
+export default Home;
