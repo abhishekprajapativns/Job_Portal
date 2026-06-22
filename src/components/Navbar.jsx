58917463dbe41887b67d9fadd0275bba95a8a28d
mito-coder <abhishek.prajapati.it@gmail.com>
2026-06-23 05:29:20 +0530

    add Login page and fix Navbar routing

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,20 +1,22 @@
+import { Link } from "react-router-dom";
+
 function Navbar() {
   return (
     <nav className="bg-blue-600 px-6 py-4 flex justify-between items-center">
       <div className="text-white text-xl font-bold">JOB-PORTAL</div>
       <div className="flex gap-6">
-        <a className="text-white hover:text-gray-200" href="#">
+        <Link className="text-white hover:text-gray-200" to="/">
           Home
-        </a>
-        <a className="text-white hover:text-gray-200" href="#">
+        </Link>
+        <Link className="text-white hover:text-gray-200" to="/findjobs">
           Find Job
-        </a>
-        <a className="text-white hover:text-gray-200" href="#">
+        </Link>
+        <Link className="text-white hover:text-gray-200" to="/login">
           Login
-        </a>
-        <a className="text-white hover:text-gray-200" href="#">
+        </Link>
+        <Link className="text-white hover:text-gray-200" to="/register">
           Register
-        </a>
+        </Link>
       </div>
     </nav>
   );
