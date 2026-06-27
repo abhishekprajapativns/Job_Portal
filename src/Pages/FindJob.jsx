e1101d77a44939e0ae5efad074878ce6282810ac
mito-coder <abhishek.prajapati.it@gmail.com>
2026-06-27 14:12:12 +0530

    add FindJobs page with job cards

--- src/Pages/FindJob.jsx
+++ src/Pages/FindJob.jsx
@@ -27,10 +27,57 @@ function FindJob() {
 
         <div className="w-3/4">
           <div className="border rounded-lg p-4 shadow-sm mb-3 ">
-            <h2 className="">React Developer</h2>
-            <p className="">TechCrop</p>
-            <p className="">Mumbai</p>
-            <button className="">Apply</button>
+            <h2 className="text-lg font-bold">React Developer</h2>
+            <p className="text-gray-500">TechCrop</p>
+            <p className="text-gray-500">Mumbai</p>
+            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mt-2">
+              Apply
+            </button>
+          </div>
+
+          <div className="border rounded-lg p-4 shadow-sm mb-3 ">
+            <h2 className="text-lg font-bold">Full Stack Developer</h2>
+            <p className="text-gray-500">Mahindra</p>
+            <p className="text-gray-500">Hydrabad</p>
+            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mt-2">
+              Apply
+            </button>
+          </div>
+
+          <div className="border rounded-lg p-4 shadow-sm mb-3 ">
+            <h2 className="text-lg font-bold">MERN Stack Developer</h2>
+            <p className="text-gray-500">Micro Sun</p>
+            <p className="text-gray-500">Mumbai</p>
+            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mt-2">
+              Apply
+            </button>
+          </div>
+
+          <div className="border rounded-lg p-4 shadow-sm mb-3 ">
+            <h2 className="text-lg font-bold">UI Designer</h2>
+            <p className="text-gray-500">IT Solution</p>
+            <p className="text-gray-500">Noida</p>
+            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mt-2">
+              Apply
+            </button>
+          </div>
+
+          <div className="border rounded-lg p-4 shadow-sm mb-3 ">
+            <h2 className="text-lg font-bold">Backend Developer</h2>
+            <p className="text-gray-500">TechCrop</p>
+            <p className="text-gray-500">Mumbai</p>
+            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mt-2">
+              Apply
+            </button>
+          </div>
+
+          <div className="border rounded-lg p-4 shadow-sm mb-3 ">
+            <h2 className="text-lg font-bold">React Developer</h2>
+            <p className="text-gray-500">TechCrop</p>
+            <p className="text-gray-500">Mumbai</p>
+            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mt-2">
+              Apply
+            </button>
           </div>
         </div>
       </div>
