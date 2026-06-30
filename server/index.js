1581a5454c837ebbb1a0fad87c89bcd23eaaa97c
mito-coder <abhishek.prajapati.it@gmail.com>
2026-06-30 20:36:09 +0530

    add auth routes

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const authRoutes = require("./routes/authRoutes");
 
 dotenv.config();
 
@@ -9,6 +10,8 @@ const app = express();
 connectDB();
 
 const PORT = 5000;
+
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
