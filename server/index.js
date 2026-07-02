0bca98361003757daf119de0286414b5e1bc781d
mito-coder <abhishek.prajapati.it@gmail.com>
2026-07-02 15:43:39 +0530

    add auth controller and fix middleware

--- server/index.js
+++ server/index.js
@@ -11,6 +11,7 @@ connectDB();
 
 const PORT = 5000;
 
+app.use(express.json());
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,9 +3,7 @@ const { registerUser } = require("../controllers/authController");
 const express = require("express");
 const router = express.Router();
 
-router.post("/register", (req, res) => {
-  res.send("Register user");
-});
+router.post("/register", registerUser);
 
 router.post("/login", (req, res) => {
   res.send("Login route working");
