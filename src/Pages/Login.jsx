fbdea17bd7097fb2e1dba6335228f12c7d2ac94f
mito-coder <abhishek.prajapati.it@gmail.com>
2026-07-04 03:21:30 +0530

    save JWT token in localStorage after login

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -13,6 +13,8 @@ function Login() {
         "http://localhost:5000/api/auth/login",
         formData,
       );
+
+      localStorage.setItem("token", res.data.token);
       alert(res.data.message);
     } catch (error) {
       alert(error.response.data.message);
@@ -22,6 +24,7 @@ function Login() {
   return (
     <div className="max-w-sm mx-auto mt-20 p-6 border rounded-lg shadow-md">
       <h1 className="text-2xl font-bold mb-4 text-center">Login</h1>
+
       <input
         className="w-full border px-3 py-2 rounded-lg mb-3 outline-none"
         type="text"
@@ -29,6 +32,7 @@ function Login() {
         value={formData.email}
         onChange={(e) => setFormData({ ...formData, email: e.target.value })}
       />
+
       <input
         className="w-full border px-3 py-2 rounded-lg mb-3 outline-none"
         type="password"
@@ -36,6 +40,7 @@ function Login() {
         value={formData.password}
         onChange={(e) => setFormData({ ...formData, password: e.target.value })}
       />
+
       <button
         onClick={handleLogin}
         className="w-full bg-blue-600 hover:bg-blue-700 text-white py-2 rounded-lg font-bold cursor-pointer"
