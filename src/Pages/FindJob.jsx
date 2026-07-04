e7010fc7c9d57d3feb29df0e2d70360e69128c8d
mito-coder <abhishek.prajapati.it@gmail.com>
2026-07-05 03:46:59 +0530

    fetch real jobs from database in FindJobs page

--- src/Pages/FindJob.jsx
+++ src/Pages/FindJob.jsx
@@ -1,4 +1,16 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 function FindJob() {
+  const [jobs, setjobs] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:5000/api/jobs")
+      .then((res) => setjobs(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+
   return (
     <div className="p-6">
       {/* Search bar */}
@@ -26,59 +38,16 @@ function FindJob() {
         {/* Job Cards - Right */}
 
         <div className="w-3/4">
-          <div className="border rounded-lg p-4 shadow-sm mb-3 ">
-            <h2 className="text-lg font-bold">React Developer</h2>
-            <p className="text-gray-500">TechCrop</p>
-            <p className="text-gray-500">Mumbai</p>
-            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mt-2">
-              Apply
-            </button>
-          </div>
-
-          <div className="border rounded-lg p-4 shadow-sm mb-3 ">
-            <h2 className="text-lg font-bold">Full Stack Developer</h2>
-            <p className="text-gray-500">Mahindra</p>
-            <p className="text-gray-500">Hydrabad</p>
-            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mt-2">
-              Apply
-            </button>
-          </div>
-
-          <div className="border rounded-lg p-4 shadow-sm mb-3 ">
-            <h2 className="text-lg font-bold">MERN Stack Developer</h2>
-            <p className="text-gray-500">Micro Sun</p>
-            <p className="text-gray-500">Mumbai</p>
-            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mt-2">
-              Apply
-            </button>
-          </div>
-
-          <div className="border rounded-lg p-4 shadow-sm mb-3 ">
-            <h2 className="text-lg font-bold">UI Designer</h2>
-            <p className="text-gray-500">IT Solution</p>
-            <p className="text-gray-500">Noida</p>
-            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mt-2">
-              Apply
-            </button>
-          </div>
-
-          <div className="border rounded-lg p-4 shadow-sm mb-3 ">
-            <h2 className="text-lg font-bold">Backend Developer</h2>
-            <p className="text-gray-500">TechCrop</p>
-            <p className="text-gray-500">Mumbai</p>
-            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mt-2">
-              Apply
-            </button>
-          </div>
-
-          <div className="border rounded-lg p-4 shadow-sm mb-3 ">
-            <h2 className="text-lg font-bold">React Developer</h2>
-            <p className="text-gray-500">TechCrop</p>
-            <p className="text-gray-500">Mumbai</p>
-            <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mt-2">
-              Apply
-            </button>
-          </div>
+          {jobs.map((job) => (
+            <div key={job._id} className="border rounded-lg p-4 shadow-sm mb-3">
+              <h2 className="text-lg font-bold">{job.title}</h2>
+              <p className="text-gray-500">{job.company}</p>
+              <p className="text-gray-500">{job.location}</p>
+              <button className="bg-blue-600 text-white px-4 py-2 rounded-lg mt-2">
+                Apply
+              </button>
+            </div>
+          ))}
         </div>
       </div>
     </div>
