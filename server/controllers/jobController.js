806fba0e10f09a416b57470a52f9891faf05e3a6
mito-coder <abhishek.prajapati.it@gmail.com>
2026-07-12 17:47:56 +0530

    add getJobById API to fetch single job details

--- server/controllers/jobController.js
+++ server/controllers/jobController.js
@@ -5,9 +5,16 @@ const getAlljobs = async (req, res) => {
   res.status(200).json(jobs);
 };
 
+const getJobById = async (req, res) => {
+  const job = await Job.findById(req.params.id);
+  if (!job) {
+    return res.status(404).json({ message: "Job not found" });
+  }
+  res.status(200).json(job);
+};
+
 const createJob = async (req, res) => {
-  const { title, company, location, salary, description, jobType, skills } =
-    req.body;
+  const { title, company, location, salary, description, jobType } = req.body;
 
   const job = new Job({
     title,
@@ -16,11 +23,10 @@ const createJob = async (req, res) => {
     salary,
     description,
     jobType,
-    skills,
   });
 
   await job.save();
   res.status(201).json({ message: "Job created successfully" });
 };
 
-module.exports = { getAlljobs, createJob };
+module.exports = { getAlljobs, createJob, getJobById };
--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -1,9 +1,14 @@
-const { getAlljobs, createJob } = require("../controllers/jobController");
+const {
+  getAlljobs,
+  createJob,
+  getJobById,
+} = require("../controllers/jobController");
 
 const express = require("express");
 const router = express.Router();
 
 router.get("/", getAlljobs);
+router.get("/:id", getJobById);
 router.post("/", createJob);
 
 module.exports = router;
