e3a896cfc03385c6b9d20921eb09f3d646a1e191
mito-coder <abhishek.prajapati.it@gmail.com>
2026-07-14 23:09:34 +0530

    add Application model

--- server/models/Application.js
+++ server/models/Application.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
 
 const applicationSchema = new mongoose.Schema({
-  job: { type: mongoose.Schema.Types.ObjectId, ref: "Job", required: true },
-  applicant: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  status: { type: String, default: "Pending" }, // Pending, Accepted, Rejected
-  appliedAt: { type: Date, default: Date.now },
+  jobId: String,
+  name: String,
+  email: String,
+  status: String,
 });
 
-module.exports = mongoose.model("Application", applicationSchema);
+const Application = mongoose.model("Application", applicationSchema);
+
+module.exports = Application;
