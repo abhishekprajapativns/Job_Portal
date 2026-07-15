9b43b421c914a0a939203ab3586aeec728f31820
mito-coder <abhishek.prajapati.it@gmail.com>
2026-07-15 14:11:38 +0530

    connect application routes in index.js

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const jobRouter = require("./routes/jobRoutes");
+const applicationRoutes = require("./routes/applicationRoutes");
 
 dotenv.config();
 
@@ -16,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/jobs", jobRouter);
+app.use("/api/applications", applicationRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
