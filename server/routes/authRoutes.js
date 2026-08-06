8a30c86c3c2eae3a6b1b35504bd4461a6507076b
Abhishek Prajapati <abhishek.prajapati.it@gmail.com>
2026-08-06 15:11:31 +0530

    feat: add protected /me route for candidate profile

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,4 +1,10 @@
-const { registerUser, loginUser } = require("../controllers/authController");
+const {
+  registerUser,
+  loginUser,
+  getMyProfile,
+} = require("../controllers/authController");
+
+const verifyToken = require("../middleware/authMiddleware");
 
 const express = require("express");
 const router = express.Router();
@@ -6,5 +12,9 @@ const router = express.Router();
 router.post("/register", registerUser);
 
 router.post("/login", loginUser);
+
+//This route is protected - verifytoken runs first to check the token, and only if it's valid does getMyProfile run next.
+
+router.get("/me", verifyToken, getMyProfile);
 
 module.exports = router;
