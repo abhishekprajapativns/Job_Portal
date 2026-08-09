c21843772690bb0255b8398371684df86462751b
Abhishek Prajapati <abhishek.prajapati.it@gmail.com>
2026-08-10 01:52:51 +0530

    feat: protect apply route and add my applications endpoint

--- server/routes/applicationRoutes.js
+++ server/routes/applicationRoutes.js
@@ -1,8 +1,18 @@
-const applyJob = require("../controllers/applicationController");
+const {
+  applyJob,
+  getMyApplications,
+} = require("../controllers/applicationController");
+
+const verifyToken = require("../middleware/authMiddleware");
 
 const express = require("express");
 const router = express.Router();
 
-router.post("/", applyJob);
+// Projected - applycation needs to know who the logged-in user is (req.userId)
+router.post("/", verifyToken, applyJob);
+
+//Project - only returns applications belonging to the longger-in user
+
+router.get("/my", verifyToken, getMyApplications);
 
 module.exports = router;
