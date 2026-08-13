ec4072a83ba1bafe6bc6ae3f7ff29bbec8f444cb
Abhishek Prajapati <abhishek.prajapati.it@gmail.com>
2026-08-13 18:09:17 +0530

    Fix Application schema: add userId field, fix jobId ref for populate

--- server/models/Application.js
+++ server/models/Application.js
@@ -1,10 +1,20 @@
 const mongoose = require("mongoose");
 
 const applicationSchema = new mongoose.Schema({
-  jobId: String,
-  name: String,
-  email: String,
-  status: String,
+  jobId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Job",
+  },
+
+  userId: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+  },
+
+  status: {
+    type: String,
+    default: "Pending",
+  },
 });
 
 const Application = mongoose.model("Application", applicationSchema);
