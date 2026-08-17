25827be16492d290d951506c93bed5d4e82ccca7
Abhishek Prajapati <abhishek.prajapati.it@gmail.com>
2026-08-17 18:58:22 +0530

    Protect job creation route with verifyToken

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -4,11 +4,13 @@ const {
   getJobById,
 } = require("../controllers/jobController");
 
+const verifyToken = require("../middleware/authMiddleware");
+
 const express = require("express");
 const router = express.Router();
 
 router.get("/", getAlljobs);
 router.get("/:id", getJobById);
-router.post("/", createJob);
+router.post("/", verifyToken, createJob);
 
 module.exports = router;
