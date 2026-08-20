951920c363a90efcbf0724d6c82d83bd427f431d
Abhishek Prajapati <abhishek.prajapati.it@gmail.com>
2026-08-20 17:57:22 +0530

    Add routes for getMyJobs, updateJob, and deleteJob

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -2,6 +2,9 @@ const {
   getAlljobs,
   createJob,
   getJobById,
+  updateJob,
+  deleteJob,
+  getMyJobs,
 } = require("../controllers/jobController");
 
 const verifyToken = require("../middleware/authMiddleware");
@@ -10,7 +13,10 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", getAlljobs);
+router.get("/my-jobs", verifyToken, getMyJobs);
 router.get("/:id", getJobById);
 router.post("/", verifyToken, createJob);
+router.patch("/:id", verifyToken, updateJob);
+router.delete("/:id", verifyToken, deleteJob);
 
 module.exports = router;
