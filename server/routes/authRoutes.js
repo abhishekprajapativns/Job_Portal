1f9675145752a939db2b4526d1f0a47924d0f4c5
Abhishek Prajapati <abhishek.prajapati.it@gmail.com>
2026-08-26 12:27:52 +0530

    Add route for resume upload

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -2,9 +2,11 @@ const {
   registerUser,
   loginUser,
   getMyProfile,
+  uploadResume,
 } = require("../controllers/authController");
 
 const verifyToken = require("../middleware/authMiddleware");
+const upload = require("../middleware/upload");
 
 const express = require("express");
 const router = express.Router();
@@ -16,5 +18,15 @@ router.post("/login", loginUser);
 //This route is protected - verifytoken runs first to check the token, and only if it's valid does getMyProfile run next.
 
 router.get("/me", verifyToken, getMyProfile);
+
+// This router is protected  and user multer.
+// verifyToken checks the login first, then upload.single("resume") saves the file, then uploadResume runs.
+
+router.post(
+  "/upload-resume",
+  verifyToken,
+  upload.single("resume"),
+  uploadResume,
+);
 
 module.exports = router;
